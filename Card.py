23c75f2c2cb074c5593e00141b21745abe1bb4d3
Wills Liou <willsliou@protonmail.com>
2021-06-01 06:24:28 +0000

    basic card class

--- Card.py
+++ Card.py
@@ -1,6 +1,22 @@
 class Card:
-    def __init__(self, suit, value):
-        self.suit = suit
+    """Represents a standard Bicycle playing card
+
+    Suits: Int 0-3
+    Rank: Int 1-13
+    # clubs (♣), diamonds (♦), hearts (♥), and spades (♠)
+    """
+    suit_names = ["♣", "♦", "♥", "♠"]
+    value_names = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
+
+    def __init__(self, value, suit):
         self.value = value
+        self.suit = suit
+
+    # def __str__(self):
+    #     very_special_values = {1: 'Ace', 11: 'Jack', 12: 'Queen', 13: 'King'}
 
 
+    # String representation
+    def __repr__(self):
+            return "Card(%s, %s)" % (self.value, self.suit)
